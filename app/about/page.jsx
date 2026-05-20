e248a29110653f33f7ae2955a6231bc9a1dfa9b2
btongheng <102462410+btongheng@users.noreply.github.com>
2026-05-20 20:54:22 +0700

    add version

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>page</div>
+    <div className="min-h-dvh bg-(--background) text-white">
+      <div className="mx-auto max-w-7xl p-4 md:p-6">
+        <h1 className="text-3xl font-bold mb-4">About Us</h1>
+        <p className="text-zinc-300 mb-6">
+          1.01
+        </p>
+      </div>
+    </div>
   )
 }
